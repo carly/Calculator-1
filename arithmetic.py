ce3b5aa18bf96d46a64f0b6ed8ac1137fb0f3051
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 15:54:24 -0700

    Final Product

--- arithmetic.py
+++ arithmetic.py
@@ -1,29 +1,40 @@
 def add(num1, num2):
     """ Adds num1 + num2 returning the result"""
     
-    return num1 + num2
+    return int(num1) + int(num2)
 
 def subtract(num1, num2):
     """ Substracts num1 - num2 returning the result """
 
-    return num1 - num2
+    return int(num1) - int(num2)
 
 def multiply(num1, num2):
     """ Multiplies num1 * num2 returning the result """
 
-    return num1 * num2
+    return int(num1) * int(num2)
 
 def divide(num1, num2):
-    pass
+    """ Divides num1 by num2 returning the result as a float."""
+
+    return float(num1) / float(num2)
+
 
 def square(num1):
-    pass
+    """ Returns the square of the number."""
+
+    return int(num1) * int(num1)
 
 def cube(num1):
-    pass
+    """Returns the cube of the number."""
+
+    return int(num1) ** 3
 
 def power(num1, num2):
-    pass
+    """ Returns the result of num1 to the power of num2."""
+
+    return int(num1) ** int(num2)
 
 def mod(num1, num2):
-    pass
+    """ Returns the remainder when num1 is divided by num2."""
+
+    return int(num1) % int(num2)
